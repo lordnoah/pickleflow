21b64392dcf3c91511fec868c007eaaedca27025
Noah Maxwell <noah.maxwell@gmail.com>
2025-12-27 15:05:48 -0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,7 @@
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: './',
-  define: {
-    'process.env.API_KEY': JSON.stringify(process.env.API_KEY)
-  }
+  base: './'
 });
